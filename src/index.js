5ef002c77dff27dfcbc874b77531afb8dd770b9e
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2024-05-30 16:31:18 +0900

    ;

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import "./index.css";
+import { useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import axios from "axios";
+import { useSocket } from "./socket/socket";
 
 function App() {
-    return <h1 className="text-blue-600">demo</h1>;
+    const { emit } = useSocket();
+
+    useEffect(() => {
+        getExample();
+    }, []);
+
+    const getExample = async () => {
+        try {
+            const response = await axios.get("http://localhost:3001");
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleClick = () => {
+        console.log("a");
+        emit("test", { data: "asd" });
+    };
+
+    return (
+        <h1 className="text-blue-600">
+            <button onClick={handleClick}>g</button>
+        </h1>
+    );
 }
 
 const domNode = document.getElementById("root");
