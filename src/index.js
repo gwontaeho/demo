5d87d00dc80267c0596db0e468d9ba4759f4ffd8
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2024-07-09 12:22:02 +0900

    ;

--- src/index.js
+++ src/index.js
@@ -4,29 +4,34 @@ import { useState } from "react";
 import { Grid } from "./grid/Grid";
 
 const useGrid = () => {
-  const getData = () => {};
-  const getChecked = () => {};
-  const getSelectedRow = () => {};
-  const getSelectedCell = () => {};
+    const getData = () => {};
+    const getCheckedRows = () => {};
+    const getSelectedRow = () => {};
+    const getSelectedCell = () => {};
 };
 
-const data = Array(99)
-  .fill(null)
-  .map((_, index) => {
-    return { index, a: "a", b: "b", c: (Math.random() * 100).toFixed() };
-  });
+const data = Array(200000)
+    .fill(null)
+    .map((_, index) => {
+        return { index, a: "a", b: "b", c: (Math.random() * 100).toFixed() };
+    });
 
 function App() {
-  const [test, setTest] = useState(0);
-  return (
-    <div>
-      {test}
-      <button onClick={() => setTest((prev) => ++prev)}>up</button>
-      <div className="p-4">
-        <Grid data={data} />
-      </div>
-    </div>
-  );
+    const [test, setTest] = useState(0);
+
+    // console.log(JSON.stringify(data));
+
+    console.log(<Grid />);
+
+    return (
+        <div>
+            {test}
+            <button onClick={() => setTest((prev) => ++prev)}>up</button>
+            <div className="p-4">
+                <Grid data={data} />
+            </div>
+        </div>
+    );
 }
 
 const domNode = document.getElementById("root");
