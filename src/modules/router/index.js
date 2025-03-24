bcf6c9b5cd42f73034d664c33d5fa7b1a1ab1dfa
gwontaeho <taeho8199@gmail.com>
2025-03-24 11:07:21 +0300

    ;

--- src/modules/router/index.js
+++ src/modules/router/index.js
@@ -16,14 +16,6 @@ const cloneDeep = (item) => {
   return obj;
 };
 
-const uuid = () => {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (char) => {
-    const random = (Math.random() * 16) | 0;
-    const value = char === "x" ? random : (random & 0x3) | 0x8;
-    return value.toString(16);
-  });
-};
-
 const RouterContext = createContext();
 const RouterStateContext = createContext();
 
