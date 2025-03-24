9813c5f9de304ec56aaf4cde9d66177c2b933d15
gwontaeho <taeho8199@gmail.com>
2025-03-24 16:14:08 +0300

    ;

--- src/module/modal/index.js
+++ src/module/modal/index.js
@@ -1,4 +1,11 @@
-import { useState, createContext, useContext, useMemo, useEffect } from "react";
+import {
+  useState,
+  createContext,
+  useContext,
+  useMemo,
+  useEffect,
+  useRef,
+} from "react";
 import { createPortal } from "react-dom";
 
 /**
@@ -24,7 +31,11 @@ const Modal = (props) => {
     onOpen,
     onClose,
   } = props;
+
   const { closeModal } = useModal();
+
+  const _modalRef = useRef(null);
+  const _headerRef = useRef(null);
 
   useEffect(() => {
     if (onOpen) {
@@ -35,6 +46,45 @@ const Modal = (props) => {
       if (onClose) {
         onClose();
       }
+    };
+  }, []);
+
+  useEffect(() => {
+    const draggable = _modalRef.current;
+    const header = _headerRef.current;
+
+    let isDragging = false;
+    let offsetX = 0;
+    let offsetY = 0;
+
+    const handleMouseDown = (event) => {
+      isDragging = true;
+      offsetX = event.clientX - draggable.offsetLeft;
+      offsetY = event.clientY - draggable.offsetTop;
+    };
+
+    const handleMouseMove = (event) => {
+      // 영역제한 추가해야함
+
+      if (!isDragging) return;
+      const x = event.clientX - offsetX;
+      const y = event.clientY - offsetY;
+      draggable.style.left = `${x}px`;
+      draggable.style.top = `${y}px`;
+    };
+
+    const handleMouseUp = () => {
+      isDragging = false;
+    };
+
+    header.addEventListener("mousedown", handleMouseDown);
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      header.removeEventListener("mousedown", handleMouseDown);
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
     };
   }, []);
 
@@ -53,11 +103,15 @@ const Modal = (props) => {
         />
       )}
       <div
+        ref={_modalRef}
         className="fixed flex flex-col w-[400px] h-[200px] left-1/2 top-1/2 border rounded-lg shadow overflow-hidden bg-gray-50"
         style={style}
       >
         {/* header */}
-        <div className="h-[40px] border-b flex items-center justify-between px-2">
+        <div
+          ref={_headerRef}
+          className="h-[40px] border-b flex items-center justify-between px-2"
+        >
           <div></div>
           <button type="button" onClick={() => closeModal(id)}>
             <svg
